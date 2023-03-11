f884b2b0af54392e96c48a73fab099c97391dcd1
TiTaniumum <36527788+TiTaniumum@users.noreply.github.com>
2023-03-11 17:56:55 +0600

    Composite Pattern Has Been Written

---
+++ Structural/CompositePattern/Composite.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern
+{
+    public abstract class Component
+    {
+        protected string Name { get; set; }
+
+        public Component(string name)
+        {
+            Name = name;
+        }
+        public abstract void Add(Component component);
+        public abstract void Remove(Component component);
+        public abstract void Display(int depth = 0);
+    }
+
+    public class Composite : Component
+    {
+        private List<Component> children = new List<Component>();
+        public Composite(string name) : base(name) { }
+
+        public override void Add(Component component)
+        {
+            children.Add(component);
+        }
+        public override void Remove(Component component)
+        {
+            children.Remove(component);
+        }
+        public override void Display(int depth = 0)
+        {
+            Console.WriteLine();
+            Console.WriteLine(new string('-', depth) + "+" + Name);
+
+            foreach (Component component in children)
+            {
+                component.Display(depth+2);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public class Leaf : Component
+    {
+        public Leaf(string name) : base(name) { }
+        public override void Add(Component component)
+        {
+            throw new Exception("Cannot add to Component::Leaf");
+        }
+        public override void Remove(Component component)
+        {
+            throw new Exception("Cannot remove anything from Component::Leaf");
+        }
+        public override void Display(int depth = 0)
+        {
+            Console.WriteLine(new string('-', depth) + "-" + Name);
+        }
+    }
+}
---
+++ Structural/CompositePattern/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CompositePattern
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Component mainComponent = new Composite("main Component");
+            Component leaf1 = new Leaf("main leaf");
+            Component leaf2 = new Leaf("main leaf");
+            Component compositeElement = new Composite("branch A");
+
+            compositeElement.Add(new Leaf("A leaf"));
+            mainComponent.Add(leaf1);
+            mainComponent.Add(leaf2);
+            mainComponent.Add(compositeElement);
+            mainComponent.Add(new Leaf("main leaf"));
+
+            Component compositeElement1 = new Composite("branch B");
+            Component compositeElement2 = new Composite("branch C");
+
+            compositeElement1.Add(new Leaf("B leaf"));
+            compositeElement1.Add(new Leaf("B leaf"));
+            compositeElement2.Add(new Leaf("C leaf"));
+            compositeElement2.Add(new Leaf("C leaf"));
+            compositeElement1.Add(compositeElement2);
+
+            mainComponent.Add(compositeElement1);
+            mainComponent.Add(new Leaf("main leaf"));
+
+
+            mainComponent.Display();
+        }
+    }
+}
